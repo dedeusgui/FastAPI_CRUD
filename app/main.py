67c340f9cd08ea281a675f9ae88ce2b33ad22ea3
dedeusgui <sernusbr@gmail.com>
2026-04-02 13:02:25 -0300

    Corrigindo primeira migration

--- app/main.py
+++ app/main.py
@@ -1,21 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from config.database import Base, engine
 from app.shared import register_exception_handlers
 from app.user.routes import router as users_router
 from app.tasks.routes import router as tasks_router
 from app.friends.routes import router as friends_router
 
 
-def create_app(*, create_tables: bool = True) -> FastAPI:
+def create_app() -> FastAPI:
     app = FastAPI()
-
-    if create_tables:
-
-        @app.on_event("startup")
-        def create_database_tables() -> None:
-            Base.metadata.create_all(bind=engine)
 
     origins = [
         "http://localhost:5173",
